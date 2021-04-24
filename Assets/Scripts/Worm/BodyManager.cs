574fd200ac6029a9edebb5baca97614dea85c35d
jvdwijk <joeyvdwijk@gmail.com>
2021-04-25 00:12:36 +0200

    chunkyWait

--- Assets/Scripts/Worm/BodyManager.cs
+++ Assets/Scripts/Worm/BodyManager.cs
@@ -9,6 +9,10 @@ public class BodyManager : MonoBehaviour
 
     [SerializeField]
     private int healthChunk = 20;
+    [SerializeField]
+    private float waitTime = 1f;
+    [SerializeField]
+    private float chunkyWait = 3f;
 
     [SerializeField, Header("Snake Parts")]
     private GameObject head;
@@ -17,20 +21,35 @@ public class BodyManager : MonoBehaviour
     [SerializeField]
     private List<BodyMovement> middleParts = new List<BodyMovement>();
 
+    private bool isCheckingParts = false;
+
     // Should be called on
     public void CheckParts(float health)
     {
-        print(health);
         int chunks = (int) health / healthChunk;
         int neededParts = chunks - middleParts.Count;
-        if (neededParts == 0) return;
-        if (neededParts > 0) AddParts(neededParts);
-        else RemoveParts(Mathf.Abs(neededParts));
+        if (isCheckingParts || neededParts == 0) return;
+
+        IEnumerator coroutine;
+        if (neededParts > 0)
+        {
+            if (health % healthChunk < chunkyWait)
+                neededParts -= 1;
+            coroutine = AddParts(neededParts);
+        }
+        else
+        {
+            coroutine = RemoveParts(Mathf.Abs(neededParts));
+        }
+
+        StartCoroutine(coroutine);
     }
 
     
-    private void AddParts(int parts)
+    private IEnumerator AddParts(int parts)
     {
+        isCheckingParts = true;
+        
         for (int i = 0; i < parts; i++)
         {
             BodyMovement part = Instantiate(partPrefab, tail.transform.position, Quaternion.identity).GetComponent<BodyMovement>();
@@ -41,11 +60,16 @@ public class BodyManager : MonoBehaviour
             tail.SetUpFollow(part.transform);
             middleParts.Add(part);
             part.transform.SetParent(gameObject.transform);
+
+            yield return new WaitForSeconds(waitTime);
         }
+        isCheckingParts = false;
     }
 
-    private void RemoveParts(int parts)
+    private IEnumerator RemoveParts(int parts)
     {
+        print("hello");
+        isCheckingParts = true;
         for (int i = 0; i < parts; i++)
         {
             if (middleParts.Count > 1)
@@ -56,7 +80,10 @@ public class BodyManager : MonoBehaviour
             BodyMovement removedPart = middleParts[middleParts.Count - 1];
             middleParts.Remove(middleParts[middleParts.Count - 1]);
             Destroy(removedPart.gameObject);
+
+            yield return new WaitForSeconds(waitTime);
         }
+        isCheckingParts = false;
     }
 
 }
