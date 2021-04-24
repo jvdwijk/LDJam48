cc813dd84eb2a6f42af37cfeeda4f275bae46f81
Coen Steuns <c.steuns@gmail.com>
2021-04-24 15:25:31 +0200

    Line of sight

---
+++ Assets/Scripts/SphereView.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SphereView : MonoBehaviour
+{
+    public abstract GameObject[] GetObjectsInSphere();
+}
