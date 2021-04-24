6a4192e6e6fd1df93f2bba7bde50bf8a2dc7afce
Coen Steuns <c.steuns@gmail.com>
2021-04-24 21:08:34 +0200

    mole atack

--- Assets/Scripts/Mole/States/AttackState.cs
+++ Assets/Scripts/Mole/States/AttackState.cs
@@ -11,22 +11,43 @@ public class AttackState : State
     private TargetRotator rotator;
 
     [SerializeField]
+    private ObjectChaser chaser;
+
+    [SerializeField]
     private float maxDistanceToPlayer;
 
     [SerializeField]
+    private float minDistanceToPlayer;
+
+    [SerializeField]
     private State chaseState;
+
+    private Health playerHealth;
+
+    private Coroutine attackRoutine;
+
+    public float damage = 5;
+    public float attackDelay = 2;
+
+    private void Start()
+    {
+        playerHealth = player.GetComponent<Health>();
+    }
 
     public override void EnterState()
     {
         base.EnterState();
         rotator.SetTarget(player);
-        print("attack!!");
+        chaser.ChaseObject(player);
+        attackRoutine = StartCoroutine(Attackroutine());
     }
 
     public override void LeaveState()
     {
         base.LeaveState();
         rotator.SetTarget(player);
+        chaser.ChaseObject(null);
+        StopCoroutine(attackRoutine);
     }
 
     public override void UpdateState(StateMachine machine)
@@ -35,6 +56,24 @@ public class AttackState : State
         if (distanceToPlayer > maxDistanceToPlayer)
         {
             machine.SetState(chaseState);
+        }
+        else if(distanceToPlayer < minDistanceToPlayer)
+        {
+            chaser.StopChasing();
+        }
+        else
+        {
+            chaser.ChaseObject(player);
+        }
+
+    }
+
+    private IEnumerator Attackroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(attackDelay);
+            playerHealth.Damage(damage);
         }
     }
 }
