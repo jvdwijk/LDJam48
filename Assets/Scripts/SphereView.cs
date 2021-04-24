735928eaf5cb681c685891343eea2cbd7355a846
Coen Steuns <c.steuns@gmail.com>
2021-04-24 15:42:43 +0200

    removed unused scripts

--- Assets/Scripts/SphereView.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SphereView : MonoBehaviour
-{
-    public abstract GameObject[] GetObjectsInSphere();
-}
