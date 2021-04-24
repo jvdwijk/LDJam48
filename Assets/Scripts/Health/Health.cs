a88d753cdfd29b43da299da7d2c37a962dcc6cba
coen <cavandiepen@gmail.com>
2021-04-24 19:11:56 +0200

    added OnChange to Health

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    private UnityEvent OnDie;
+    public UnityEvent OnDie;
+    public UnityEvent OnChange;
 
     private float value = 100;
     private float max = 100;
@@ -19,12 +20,14 @@ public class Health : MonoBehaviour
     public void Heal(float input)
     {
         value = Mathf.Min(value + input, max);
+        OnChange.Invoke();
     }
 
     public void Damage(float input) //todo: bool ignore damage reduction
     {
         value -= input;
         //todo: damage reduction
+        OnChange.Invoke();
     }
 
     void Update()
