5f7d8c6170fa7742ca7d4b0738e455d905bca674
jvdwijk <joeyvdwijk@gmail.com>
2021-04-26 00:31:49 +0200

    Main scene

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    private void Awake()
+    {
+#if UNITY_WEBGL
+        gameObject.SetActive(false);
+#endif
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
