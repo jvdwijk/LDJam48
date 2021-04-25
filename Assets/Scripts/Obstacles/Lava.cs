e5c2fd706cc06075d6a9a2398894bac0027fb372
jvdwijk <joeyvdwijk@gmail.com>
2021-04-25 16:31:49 +0200

    hello!

--- Assets/Scripts/Obstacles/Lava.cs
+++ Assets/Scripts/Obstacles/Lava.cs
@@ -15,7 +15,6 @@ public class Lava : MonoBehaviour
             float distance = Vector3.Distance(hitCollider.transform.position, transform.position);
             if (distance < damageRadius && hitCollider.TryGetComponent(out Health health)) //filter only Gameobjects within range and with the script health
             {
-                print("hello");
                 health.Damage(damageRate * Time.deltaTime * (1 - distance / damageRadius)); //less damage over time the further you are from the mine
             }
         }
