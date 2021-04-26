79298b05e04359b84cae7b3d37fc60166feea1ab
coen <cavandiepen@gmail.com>
2021-04-26 12:38:51 +0200

    added improved spawning

--- Assets/Scripts/Spawning/SpawnThings.cs
+++ Assets/Scripts/Spawning/SpawnThings.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class SpawnThings : MonoBehaviour
 {
+    [SerializeField]
+    private SpawnPool spawnPool;
     public SpawningChance[] spawningChances;
     public float spawnDistance;
 
-    public GameObject wormhead;
-    
+    public GameObject wormHead;
+
+    private void Start()
+    {
+        
+    }
+
     private void Update()
     {
         foreach (SpawningChance chancy in spawningChances) //foreach prefab
@@ -16,7 +23,7 @@ public class SpawnThings : MonoBehaviour
             float spawnRate = 0;
             for (int i = chancy.depthChance.Length - 1; i >= 0; i--) //check depth to determine chance
             {
-                if (chancy.depthChance[i].depth <= -wormhead.transform.position.y)
+                if (chancy.depthChance[i].depth <= -wormHead.transform.position.y)
                 {
                     spawnRate = chancy.depthChance[i].rate;
                     break;
@@ -29,18 +36,19 @@ public class SpawnThings : MonoBehaviour
         }
     }
 
-    public void Spawn(GameObject prefab)
+    public void Spawn(GameObject prefab) //spawns things in a half circle in front of the worm head at spawnDistance
     {
-        GameObject obj = Instantiate(prefab);
-        //obj.transform.position = wormhead.transform.position + -wormhead.transform.up * spawnDistance + wormhead.transform.right * Random.Range(-spawnWidth, spawnWidth);
+        GameObject obj = spawnPool.PullObject(prefab);
         
-        float ang = -wormhead.transform.eulerAngles.z + Random.value * 180 + 90;
+        float ang = -wormHead.transform.eulerAngles.z + Random.value * 180 + 90;
         Vector3 pos;
-        pos.x = wormhead.transform.position.x + spawnDistance * Mathf.Sin(ang * Mathf.Deg2Rad);
-        pos.y = wormhead.transform.position.y + spawnDistance * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.x = wormHead.transform.position.x + spawnDistance * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = wormHead.transform.position.y + spawnDistance * Mathf.Cos(ang * Mathf.Deg2Rad);
         pos.z = 0;
         obj.transform.position = pos;
 
-        print(wormhead.transform.eulerAngles.z);
+        Despawn despawn = obj.GetComponent<Despawn>();
+        despawn.despawnDistance = spawnDistance + 1;
+        despawn.wormHead = wormHead;
     }
 }
