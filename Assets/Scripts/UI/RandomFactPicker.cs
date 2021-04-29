e95efffb05d0f4c5d3256888e2716e5f65a26ee7
jvdwijk <joeyvdwijk@gmail.com>
2021-04-29 20:45:15 +0200

    mole fixed after 3k

--- Assets/Scripts/UI/RandomFactPicker.cs
+++ Assets/Scripts/UI/RandomFactPicker.cs
@@ -10,10 +10,27 @@ public class RandomFactPicker : MonoBehaviour
     [SerializeField]
     private List<string> facts;
 
+    [SerializeField]
+    private float upPosition, speedySpeed;
+    private float contertje = 0, startY;
+
+
     private void Start()
     {
+        startY = transform.localPosition.y;
         factText = GetComponent<TextMeshProUGUI>();
         int factNumber = Random.Range(0, facts.Count);
         factText.text = facts[factNumber];
+        StartCoroutine(lerpPosition());
+    }
+
+    private IEnumerator lerpPosition()
+    {
+        while (true)
+        {
+        contertje += Time.deltaTime * speedySpeed;
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Sin(contertje) * upPosition + startY, 0);
+        yield return null;
+        }
     }
 }
